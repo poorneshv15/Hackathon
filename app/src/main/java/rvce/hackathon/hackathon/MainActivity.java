94a3f35ababcf30f9d50232843495a1e263f66b3
Rohit Goudar <rohitgoudar444@gmail.com>
2017-03-25 23:44:50 +0530

    slpash

--- app/src/main/java/rvce/hackathon/hackathon/MainActivity.java
+++ app/src/main/java/rvce/hackathon/hackathon/MainActivity.java
@@ -1,5 +1,7 @@
 package rvce.hackathon.hackathon;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 3000);
     }
 }
